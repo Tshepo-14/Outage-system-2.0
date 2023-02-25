01f42410442dbdb4535431487a5df2ed898195de
Tshepo Molaoa <51974901+Tshepo-14@users.noreply.github.com>
2023-02-25 19:53:05 +0200

    Update epiz_33314110_outagesystem.sql

--- epiz_33314110_outagesystem.sql
+++ epiz_33314110_outagesystem.sql
@@ -1,10 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: sql309.epizy.com
--- Generation Time: Feb 25, 2023 at 12:43 PM
--- Server version: 10.3.27-MariaDB
 -- PHP Version: 7.2.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -12,19 +5,6 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `epiz_33314110_outagesystem`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `admin`
 --
 
@@ -136,33 +116,19 @@ INSERT INTO `user` (`city`, `counts`) VALUES
 ('Johannesburg', 1),
 ('Johannesburg', 1);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `outages`
 --
 ALTER TABLE `outages`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tech`
+
 --
 ALTER TABLE `tech`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `outages`
 --
 ALTER TABLE `outages`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
